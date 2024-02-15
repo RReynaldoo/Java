fcb121d32dba1d632c5ab76e84219e78fcf9de5d
RReynaldoo <157304105+RReynaldoo@users.noreply.github.com>
2024-02-15 10:56:32 -0500

    jaja

--- Java/CeOverloadingMethods.java
+++ Java/CeOverloadingMethods.java
@@ -28,5 +28,16 @@ class HelloWorld {
     public static void getValue(double smth){
         smth = smth / 2;
         System.out.println(smth);
+//------------------------------------------------
+
+/*Ambiguity is what happens when the program cant determine which method to execute. This usually happens
+when, for example, you have the following:
+methodCall(double x, int y)
+methodCall(int x, double y)
+
+Since it is possible to enter two ints and then it ascends to double, the program will give an error.
+In this cases, either avoid doing this and just use the first example, or think of every possibility and
+create a method for that
+*/
     }
 }
